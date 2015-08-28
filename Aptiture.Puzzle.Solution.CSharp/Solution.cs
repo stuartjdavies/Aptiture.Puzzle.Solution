25a75b8861c16686c53d6e4d3ceec6bbea7ed0b4
stuartjdavies <stuart_j_davies@hotmail.com>
2015-08-28 16:38:20 +1000

    Removed unneed using statements.

--- Aptiture.Puzzle.Solution.CSharp/Solution.cs
+++ Aptiture.Puzzle.Solution.CSharp/Solution.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 /// <summary>
 /// A Student can be enrolled in 4 subjects
